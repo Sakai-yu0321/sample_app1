54076ea2f6b5872bb2f105b94e86911c68511bff
Sakai-yu0321 <on99.yu.sakai@gmail.com>
2021-05-09 08:49:18 +0900

    投稿編集機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.order(created_at: :desc)
+    @posts = Post.all.order(updated_at: :desc)
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
   end
 
   def new
@@ -14,6 +18,22 @@ class PostsController < ApplicationController
       redirect_to("/posts/index")
     else
       render("posts/new")
+    end
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+    @post.title = params[:title]
+    @post.content = params[:content]
+    if @post.save
+      redirect_to("/posts/index")
+      flash[:notice] = "編集が完了しました"
+    else
+      render("posts/edit")
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   post "posts/create" => "posts#create"
   get "posts/new" => "posts#new"
   get "posts/index" => "posts#index"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  post "posts/:id/destroy" => "posts#destroy"
+  get "posts/:id" => "posts#show"
 
   get "/" => "home#top"
   get "/about" => "home#about"
